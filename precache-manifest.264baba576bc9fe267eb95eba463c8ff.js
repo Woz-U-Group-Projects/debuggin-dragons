9d267498d4e6e32ac117b2ec4aea8a5bbe9b8802
Alex <alexrabin@me.com>
2021-12-07 20:55:53 -0700

    Updates

---
+++ precache-manifest.264baba576bc9fe267eb95eba463c8ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8f6b8f3bcd328267499ceb55e19a33",
+    "url": "/dragazon/index.html"
+  },
+  {
+    "revision": "2de393345a836cdd3dc2",
+    "url": "/dragazon/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "88c8302f75211c47d91b",
+    "url": "/dragazon/static/css/main.b1376436.chunk.css"
+  },
+  {
+    "revision": "2de393345a836cdd3dc2",
+    "url": "/dragazon/static/js/2.ec6e4eeb.chunk.js"
+  },
+  {
+    "revision": "88c8302f75211c47d91b",
+    "url": "/dragazon/static/js/main.f13f4080.chunk.js"
+  },
+  {
+    "revision": "9c23fab70fcc8ad76ee0",
+    "url": "/dragazon/static/js/runtime~main.ae59200e.js"
+  },
+  {
+    "revision": "bccddc374dff39cb82c276e57cc5af6a",
+    "url": "/dragazon/static/media/dragazonlogoLight.bccddc37.png"
+  },
+  {
+    "revision": "4581bcedd3e94b5f4f7da67eee6e5fc2",
+    "url": "/dragazon/static/media/newitem.4581bced.png"
+  },
+  {
+    "revision": "4084d4d0700129b7159dc97f7b0abe9c",
+    "url": "/dragazon/static/media/textheaderligred.4084d4d0.png"
+  }
+]);
